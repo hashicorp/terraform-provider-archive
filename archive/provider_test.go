8b867c385c744433595c2faf23fef97347e9bf5c
Radek Simko <radek.simko@gmail.com>
2020-02-04 15:45:04 +0100

    Opt into binary driver

--- archive/provider_test.go
+++ archive/provider_test.go
@@ -3,12 +3,19 @@ package archive
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 var testProviders = map[string]terraform.ResourceProvider{
 	"archive": Provider(),
+}
+
+func TestMain(m *testing.M) {
+	acctest.UseBinaryDriver("archive", Provider)
+	resource.TestMain(m)
 }
 
 func TestProvider(t *testing.T) {
