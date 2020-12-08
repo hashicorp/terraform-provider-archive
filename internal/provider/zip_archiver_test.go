fda98e0ed80ec14f2cab23cbeaa11176af26f16b
moajo <mimirosiasd@gmail.com>
2020-12-08 23:10:56 +0900

    modify TestZipArchiver_Dir_Exclude_With_Directory

--- internal/provider/zip_archiver_test.go
+++ internal/provider/zip_archiver_test.go
@@ -101,14 +101,15 @@ func TestZipArchiver_Dir_Exclude(t *testing.T) {
 func TestZipArchiver_Dir_Exclude_With_Directory(t *testing.T) {
 	zipfilepath := "archive-dir.zip"
 	archiver := NewZipArchiver(zipfilepath)
-	if err := archiver.ArchiveDir("./test-fixtures/", []string{"test-dir", "test-dir2/file2.txt"}); err != nil {
+	if err := archiver.ArchiveDir("./test-fixtures/test-dir3", []string{"file1.txt", "test-subdir/file2.txt"}); err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
 
 	ensureContents(t, zipfilepath, map[string][]byte{
-		"test-dir2/file1.txt": []byte("This is file 1"),
-		"test-dir2/file3.txt": []byte("This is file 3"),
-		"test-file.txt":       []byte("This is test content"),
+		"test-subdir/file1.txt": []byte("This is file 1"),
+		"test-subdir/file3.txt": []byte("This is file 3"),
+		"file2.txt":             []byte("This is file 2"),
+		"file3.txt":             []byte("This is file 3"),
 	})
 }
 
