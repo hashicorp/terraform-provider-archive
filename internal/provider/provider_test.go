40fdc992401f164fe67564a411ec268cf3950c0d
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-11-07 14:34:22 +0000

    Ignore linting on test providers (#112)

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+//nolint:unparam
 var testProviders = map[string]func() (*schema.Provider, error){
 	"archive": func() (*schema.Provider, error) {
 		return Provider(), nil
