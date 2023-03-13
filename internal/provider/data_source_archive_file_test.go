3df976b4e54b4cff3dabf327678dfe0000456241
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-03-13 13:16:57 +0000

    all: Acceptance tests have been migrated to https://github.com/hashicorp/terraform-plugin-testing (#192)
    Reference: https://github.com/hashicorp/terraform-providers-devex-internal/issues/135

--- internal/provider/data_source_archive_file_test.go
+++ internal/provider/data_source_archive_file_test.go
@@ -7,8 +7,8 @@ import (
 	"regexp"
 	"testing"
 
-	r "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	r "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccArchiveFile_Basic(t *testing.T) {
